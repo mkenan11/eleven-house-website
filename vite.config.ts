8aa4b39fc4225b8081fe576d33003db0b8e52eca
Kanan Mammadov <mammedovkenan1117@gmail.com>
2026-08-16 11:22:16 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/eleven-house-website/',
   plugins: [react()],
   resolve: {
     alias: {
